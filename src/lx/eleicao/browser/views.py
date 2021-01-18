8fd0e6e145655a4f0831085c68f433d40acf0c97
Lucas Aquino <luxcas@gmail.com>
2021-01-18 19:00:15 -0300

    tela de votacao

--- src/lx/eleicao/browser/views.py
+++ src/lx/eleicao/browser/views.py
@@ -1,9 +1,9 @@
 # encoding: utf-8
 from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
-
+from Products.CMFPlone.utils import _createObjectByType
 from plone import api
-
+from datetime import datetime
 import logging
 
 
@@ -17,14 +17,19 @@ class VotacaoView(BrowserView):
         self.url_sucess = self.context.absolute_url()
         self.utils = getToolByName(self.context, 'plone_utils')
         self.tela_votacao = self.context.absolute_url()+"/votacao-conselho-deliberativo"
+        self.tela_posvotacao = self.context.absolute_url()+"/pos-votacao"
 
         if 'codigo_socio' in request.form:
             codigo_socio_ = request.form['codigo_socio']
             request.set('codigo_socio', codigo_socio_)
+        
 
     def __call__(self):
         if "codigo_socio" in self.request.form:
             self.validarCodigo(self.request.form['codigo_socio'])
+
+        if "form_votar" in self.request.form:
+            self.validarVoto(self.request.form, 'errado')
         return self.index()
 
     def validarCodigo(self, codigo):
@@ -34,8 +39,19 @@ class VotacaoView(BrowserView):
             getToolByName(self.context, 'plone_utils').addPortalMessage("Informar o código.", type='error')
             # codigo = self.request.get('codigo_socio', None)
         else:
+            self.request.set('codigo_socio', codigo)
+            self.request['codigo_socio'] = codigo
+            self.request.response.setHeader('codigo_socio', codigo)
             self.request.response.redirect(self.tela_votacao)
 
+    def validarVoto(self, voto, codigo):
+        """Validação do codigo socio.
+        """
+        if 'voto' not in voto.keys():
+            getToolByName(self.context, 'plone_utils').addPortalMessage("Selecionar uma chapa, ou votar nulo.", type='error')
+        else:
+            self.setVoto(self.request.form['voto'])
+            
 
     def getCandidatos(self):
         lista_candidatos = []
@@ -52,3 +68,26 @@ class VotacaoView(BrowserView):
                     }
             lista_candidatos.append(dado)
         return lista_candidatos
+
+    def setVoto(self, voto_eleitor):
+        # import pdb; pdb.set_trace()
+        portal = api.portal.get()['eleicao-conselho-deliberativo']['urna']
+        dados_form = self.request.form
+        if dados_form['voto'] == 'nulo':
+            anulado = True
+        else:
+            anulado = False
+
+        id_voto = "voto-{0}".format(datetime.now().strftime("%d%m%y%f"))
+        titulo_voto = "{0}-{1}-{2}".format(id_voto, dados_form['voto'], datetime.now().strftime("%d/%m/%y"))
+        _createObjectByType('voto', 
+                            portal, 
+                            id=id_voto, 
+                            title=titulo_voto,
+                            id_chapa=dados_form['voto'],
+                            codigo_eleitor='avulso',
+                            voto_nulo=anulado,
+                            data_voto=datetime.now()
+                            )
+
+        self.request.response.redirect(self.tela_posvotacao)
